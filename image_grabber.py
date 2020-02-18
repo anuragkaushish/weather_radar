f101e7d02ff4321178fb5c5b4b6b53d5a0b4db9a
anuragkaushish <33181820+anuragkaushish@users.noreply.github.com>
2020-02-17 19:36:08 -0600

    Update image_grabber.py

--- image_grabber.py
+++ image_grabber.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3.5
 # -*- coding: utf-8 -*-
 """
-Created on Wed May 23 09:16:29 2018
+Created on Wed May 23 09:16:29 2019
 
-@author: Manish Sharma
+@author: Anurag Sharma
 """
 
 import paramiko
@@ -15,7 +15,7 @@ class ImageGrabber:
         local_file = '/home/sanand/radar/images/{}.gif'.format(image_name)
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh_client.connect(hostname='139.59.16.54',username='root',password='dentintheuniverse',port=22)
+        ssh_client.connect(hostname='139.59.16.54',username='*******',password='*********',port=22)
         ftp_client=ssh_client.open_sftp()
         ftp_client.get(remote_file,local_file)
         ftp_client.close()
@@ -23,10 +23,10 @@ class ImageGrabber:
     
     def server_to_local(self,image_type,image_name):
         remote_file = '/home/amss/amss_images/{}_dlh/{}.gif'.format(image_type,image_name)
-        local_file = 'C:/Users/Manu/Documents/Python Scripts/RadarImages/server/images/{}.gif'.format(image_name)
+        local_file = 'C:/Users/anurag/Documents/Python Scripts/RadarImages/server/images/{}.gif'.format(image_name)
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh_client.connect(hostname='139.59.16.54',username='root',password='dentintheuniverse',port=22)
+        ssh_client.connect(hostname='139.59.16.54',username='*****',password='*********',port=22)
         ftp_client=ssh_client.open_sftp()
         ftp_client.get(remote_file,local_file)
         ftp_client.close()
