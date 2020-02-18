29a960455570625b8ce03a3b340aad3e49ef6524
anuragkaushish <33181820+anuragkaushish@users.noreply.github.com>
2020-02-17 19:34:40 -0600

    Update db.py

--- db.py
+++ db.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3.5
 # -*- coding: utf-8 -*-
 """
-Created on Tue May 29 16:42:53 2018
+Created on Tue May 29 16:42:53 2019
 
-@author: Manish Sharma
+@author: Anurag Sharma
 """
 
 import pymysql
@@ -13,11 +13,11 @@ class DB(object):
     
     def getConnection(self):
         # Open database connection
-        db = pymysql.connect("139.59.42.147","albert","albert123","bses_consumption")
+        db = pymysql.connect("139.59.42.147","******","********","bses_consumption")
         # prepare a cursor object using cursor() method
         # cursor = db.cursor()
         return db
     
     def getEngine(self):
-        engine = create_engine('mysql+pymysql://albert:albert123@139.59.42.147:3306/bses_consumption', echo=False)
+        engine = create_engine('mysql+pymysql://******:********@********:3306/bses_consumption', echo=False)
         return engine
